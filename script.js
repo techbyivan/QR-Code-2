9f7d397108c84d1ec85907d5e383661b98d8db71
Ivan Davis <techbyivan@users.noreply.github.com>
2020-07-10 16:12:39 -0400

    QR Code Gen.

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-let input = document.querySelector('input');
-let button = document.querySelector('button');
+let input = document.querySelector("input");
+let button = document.querySelector("button");
 
+let qrcode = new QRCode(document.querySelector("#qrcode"), {
+	width: 250,
+	height: 250,
+	colorDark: "#000000",
+	colorLight: "#ffffff",
+	correctLevel: QRCode.CorrectLevel.H,
+});
 
-let qrcode = new QRCode(document.querySelector('#qrcode'), {
-    width: 250, 
-    height: 250,
-    colorDark: "#000000",
-    colorLight: "#ffffff",
-    correctLevel: QRCode.CorrectLevel.H
-})
-
-button.addEventListener('click', () => {
-    let inputValue = input.value;
-    qrcode.makeCode(inputValue)
-})
+button.addEventListener("click", () => {
+	let inputValue = input.value;
+	qrcode.makeCode(inputValue);
+});
